443ed804c5f526ae937907370c0bbd376abc9346
muhammadriyansadiq <muhammadriyansadiq77@gmail.com>
2023-10-25 18:07:57 +0500

    added

--- app.js
+++ app.js
@@ -9,17 +9,12 @@ b.addEventListener("click",function () {
     c = c.filter(char => char !== ',' && char !== ' ' && char !== '.' && char !== "'").join("");
     console.log(f);
     if(c === f){// these for all includung special characters and spaces(remove spaces and special chr then check) if you written bb so that check only alphanumeric data
-
         swal(`Yes You have written Palindrome words ${r}`);
     }
     else{
         swal(`This is not a  Palindrome words ${r}`);
-
     }
-  
 })
-
-
 
 
 
